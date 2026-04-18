cc20e5803b5d56e25d9e831e365813513dcb1fcf
Ziying Mi <miziying2008@gmail.com>
2026-04-18 02:50:43 +0000

    order_id_error_adjust

--- models/base_model/returns.sql
+++ models/base_model/returns.sql
@@ -23,5 +23,24 @@ cleaned as (
     from source
 
 )
+-- ,deduplicated as (
+
+--     select *,
+--         row_number() over (
+--             partition by order_id
+--             order by returned_date asc
+--         ) as rn
+--     from cleaned
+
+-- )
+
+-- select
+--     order_id,
+--     returned_date,
+--     is_refunded,
+--     _fivetran_synced
+-- from deduplicated
+-- where rn = 1
 
 select * from cleaned
+
